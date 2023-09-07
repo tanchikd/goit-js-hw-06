4d070b739417ce496c5baec3424de8b993c255c6
Tania <doroschenko.tanya@gmail.com>
2023-09-07 21:46:03 +0300

    second commit

--- js/task-01.js
+++ js/task-01.js
@@ -3,10 +3,6 @@ const allItemsSum = allItems.length;
 console.log(`Number of categories: ${allItemsSum}`);
 
 allItems.forEach((element) => {
-  let elementTitle = element.querySelector("h2").textContent;
-  let titleElements = element.querySelectorAll("li");
-  let titleElemntsSum = titleElements.length;
-
-  console.log(`Category: ${elementTitle}`);
-  console.log(`Elements ${titleElemntsSum}`);
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${element.lastElementChild.children.length}`);
 });
